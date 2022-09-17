2fd5e9a6f17262c70c00dafcce29cf6cc1629ac6
Manuch Sadri <colonquebang@hotmail.com>
2022-09-17 01:09:29 -0700

    completed steps 1-11

--- app.js
+++ app.js
@@ -177,7 +177,7 @@ Student.prototype.greeting = function() {
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this method to break!
-// console.log(joe.greeting());
+console.log(joe.greeting());
 
 // TODO: After viewing the previous console.log(), return the code to a working state.
 
@@ -188,7 +188,7 @@ Student.courseName = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(Student.courseName());
+console.log(Student.courseName());
 
 
 
@@ -199,17 +199,19 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// joe.scope();
+joe.scope();
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// joe.scopeArrow();
+joe.scopeArrow();
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+// in this context the "this" is the Student object joe
+
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+// in this context the "this" is the Window object
+
 // 3. Explain why "this" is different when an arrow function is used.
-//
+// Arrow functions don't have their own bindings to `this`, `arguments` or `super`, and should not be used as methods, and therefore Arrow functions cannot be used as constructors.
